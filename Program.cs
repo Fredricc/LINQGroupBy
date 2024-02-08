787a2e6ee890d7399b5ea9754099b404c91b5579
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-08 18:36:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LINQGroupBy
+{
+    public record Employee(string FirstName, string LastName, string Department);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>
+            {
+                new Employee("Nicholas", "Mamau", "Development"),
+                new Employee("Ken", "Njuguna", "Sales"),
+                new Employee("Ken", "Njenga", "Sales"),
+                new Employee("Paul", "Kiprotech", "Project Management"),
+                new Employee("Isaac", "Soita","Project Management"),
+                new Employee("James", "Kimani", "Development")
+            };
+
+            var personsByDepartment = employees.GroupBy(person => person.Department);
+
+            foreach (var department in personsByDepartment) 
+            {
+                Console.WriteLine($"Department: {department.Key}");
+
+                foreach (var employee in department) 
+                {
+                    Console.WriteLine($"    {employee.FirstName} {employee.LastName}");
+                }
+
+            }
+            
+        }
+    }
+}
